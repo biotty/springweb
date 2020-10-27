b5edcf5b6b3290d6b89c4b6e3bbe4f288c6145a2
Christian Oeien <christian.oien@gmail.com>
2020-10-27 00:24:45 -0500

    collect letters on bar

--- cmd/springweb-wasm/main.go
+++ cmd/springweb-wasm/main.go
@@ -10,66 +10,59 @@ import (
 )
 
 const (
-	defaultK            = 1.  // note: K (and dependent armK) seems depending on dotSize
-	armKFactor          = 1e3 //       as the model operates on the distances provided directly
-	minK                = defaultK * .15
+	defaultK            = 1.
+	armKFactor          = 1e3
+	minK                = defaultK * .25
 	maxK                = defaultK * 5
 	defaultMass         = 5e-3
-	minMass             = defaultMass * .15
+	minMass             = defaultMass * .25
 	maxMass             = defaultMass * 5
 	platformBounce      = .5
 	platformSpeed       = 9
 	platformStick       = .3
-	gravity             = 1e3  // note: dep. on dotSize.  make independent by multiplying at calc.
-	maxWheelForce       = 2.0  //       idem
-	maxWheelVelocity    = 2e1  // note: measured in dotSize as unit, so constant not dep. on dotSize
-	wheelGyrationFactor = 1 // note: deps as explained
-	wheelDriveArmFactor = 1 //       idem .. but maybe not this one, as it is purely angular
-	sizeFactor          = 5e-2 //       (but seems the related maxWheelForce is dep on scale ..)
-	sizeButtonClick     = 5.
+	gravity             = 7e2
+	maxWheelForce       = 1.1
+	maxWheelVelocity    = 1e1
+	wheelGyrationFactor = 1
+	wheelDriveArmFactor = 1
 	voidColor           = "#ffd"
 	barColor            = "#bd3"
-	buttonColor         = "#451"
-	dotColor            = "#42d"
-	selectedDotColor    = "#87e"
+	barTextColor        = "#451"
+	barTextHighColor    = "#fff"
 	lineColor           = "rgba(96, 32, 0, 0.2)"
-	selectedLineColor   = "rgba(255, 96, 16, 0.5)"
 	platformColor       = "rgba(0, 128, 128, 0.5)"
 	rightForceColor     = "rgba(0, 0, 255, 0.2)"
 	leftForceColor      = "rgba(255, 0, 0, 0.2)"
+	letterColor         = "rgba(64,32,200,.5)"
+	letterCupColor      = "rgba(64,32,200,.75)"
 )
 
-func (a *anim) newDot(x, y float64) {
-	m := a.lastMass()
+func (a *anim) vary() float64 {
+	return .8 + .4*a.rands.Float64()
+}
+
+func (a *anim) newDotM(x, y, m float64) {
+	m *= a.vary()
 	r := a.dotRadius(m)
 	a.dots[a.nDots] = springweb.NewNode(x, y, r, m)
 	a.nDots++
 }
 
-func (a *anim) newLine(i, j int) {
-	k := a.lastK()
+func (a *anim) newDot(x, y float64) {
+	a.newDotM(x, y, defaultMass)
+}
+
+func (a *anim) newLineK(i, j int, k float64) {
+	k *= a.vary()
 	a.dots[i].NewSpring(&a.dots[j], k, armKFactor*k)
 }
 
-func (a *anim) findDot(x, y float64) int {
-	outsideAllow := 1.7
-	if !a.running {
-		outsideAllow = 1.3
+func (a *anim) newLine(i, j int) {
+	if i <= j { // contract: springweb
+		log("newLine", i, j)
+		return
 	}
-	for i := 0; i < a.nDots; i++ {
-		d := a.dots[i]
-		r := a.dotRadius(d.M) * outsideAllow
-		if math.Pow(x-d.X, 2)+math.Pow(y-d.Y, 2) <= math.Pow(r, 2) {
-			return i
-		}
-	}
-	return -1
-}
-
-type wheel struct {
-	angleVelocity float64
-	angle         float64
-	onPlatform    int
+	a.newLineK(i, j, defaultK)
 }
 
 type platform struct {
@@ -121,28 +114,59 @@ func (p *platform) bounce(d *springweb.Node, depth float64) {
 	d.Y += depth * p.surfaceY
 }
 
+type wheel struct {
+	angleVelocity float64
+	angle         float64
+	onPlatform    int
+}
+
 type anim struct {
 	width, height, dotSize float64
-	dots, resetNodes       []springweb.Node
+	dots                   []springweb.Node
 	nDots                  int
-	nGassDots              int
+	iLetterDots            int
 	nCarDots               int
-	selectedDot            int
-	dragging               bool
 	ctx                    js.Value
 	images                 []js.Value
 	callback               js.Func
 	lastCall               time.Time
 	deltaT                 float64
-	running                bool
-	keyisdown              bool
-	viewX, resetViewX      float64
+	viewX                  float64
 	wheelForce             float64
 	wheels                 []wheel
 	nWheels                int
 	platforms              []platform
 	nPlatforms             int
+	alienLetters           []int
+	nLetterAliens          int
+	haveLetters            []bool
 	rands                  *rand.Rand
+}
+
+func (a *anim) setCallback() {
+	a.callback = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		t := time.Now()
+
+		deltaT := 1e-9 * float64(t.Sub(a.lastCall))
+		if deltaT < .3 || a.deltaT == 0 {
+			a.deltaT = deltaT
+		}
+		a.lastCall = t
+
+		springweb.Step(a.dots[:a.nDots], a.deltaT)
+		a.wheelsStep()
+		a.lettersStep()
+		a.platformsStep(deltaT)
+		a.gravityStep()
+		a.viewBorderStep()
+		a.viewScrollStep()
+		a.worldCycle()
+		a.drawView()
+		a.drawControl()
+
+		js.Global().Call("requestAnimationFrame", a.callback)
+		return nil
+	})
 }
 
 func newAnim(width, height, dotSize float64, nNodes int) *anim {
@@ -156,54 +180,23 @@ func newAnim(width, height, dotSize float64, nNodes int) *anim {
 		doc.Call("getElementById", "image1"),
 	}
 	ctx := elem.Call("getContext", "2d")
+	ctx.Set("font", "15px Arial")
 	a := anim{width, height, dotSize,
-		make([]springweb.Node, nNodes), nil, 0, 0, 0, 0, false,
-		ctx, images, js.Func{}, time.Time{}, 0, false, false, 0, 0, 0,
-		nil, 2, nil, 15, rand.New(rand.NewSource(time.Now().UnixNano()))}
+		make([]springweb.Node, nNodes), 0, 0, 0,
+		ctx, images, js.Func{}, time.Time{}, 0, 0, 0,
+		nil, 2, nil, 15, nil, 7, nil,
+		rand.New(rand.NewSource(time.Now().UnixNano()))}
 
 	a.wheels = make([]wheel, a.nWheels)
 	a.platforms = make([]platform, a.nPlatforms)
-
-	a.clear()
-	a.callback = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		if !a.running {
-			return nil
-		}
-		t := time.Now()
-		if a.running {
-			x, y := a.draggedDotPosition()
-			deltaT := 1e-9 * float64(t.Sub(a.lastCall))
-			if a.deltaT == 0 || deltaT < a.deltaT*9 {
-				a.deltaT = deltaT
-			}
-			springweb.Step(a.dots[:a.nDots], a.deltaT)
-			for i := 0; i < a.nWheels; i++ {
-				if i < a.nDots {
-					a.wheelRotation(i)
-				}
-			}
-			a.platformsStep(deltaT)
-			a.gravityStep()
-			a.viewBorderStep()
-			a.viewScrollStep()
-			a.worldCycle()
-			a.positionDraggedDot(x, y)
-			a.drawView()
-			a.drawControl()
-		}
-		a.lastCall = t
-		js.Global().Call("requestAnimationFrame", a.callback)
-		return nil
-	})
+	a.alienLetters = make([]int, a.nLetterAliens)
+	a.haveLetters = make([]bool, 26)
+	a.setCallback()
 	return &a
 }
 
-func (a *anim) buttonHeight() float64 {
+func (a *anim) barHeight() float64 {
 	return a.dotSize * 2
-}
-
-func (a *anim) buttonRight(i int) float64 {
-	return float64(i+1) * a.width / 3
 }
 
 func (a *anim) dotRadius(mass float64) float64 {
@@ -214,43 +207,32 @@ func (a *anim) lineWidth(k float64) float64 {
 	return a.dotSize * k / (2 * defaultK)
 }
 
-func drawTriangle(ctx js.Value, i int, x, y float64) {
-	var px, py []float64
-	if i == 0 {
-		px = []float64{x - y/2, x + y/2, x - y/2}
-		py = []float64{0, y / 2, y}
-	} else {
-		px = []float64{x - y/2, x + y/2, x}
-		if i == 1 {
-			py = []float64{y / 2, y / 2, y}
-		} else {
-			py = []float64{y / 2, y / 2, 0}
+func alphabetLetter(i int) int {
+	return i + 65
+}
+
+func (a *anim) drawLettersHave(have bool, color string) {
+	a.ctx.Call("save")
+	a.ctx.Set("fillStyle", color)
+	y := a.barHeight()
+	s := a.dotSize / 19
+	n := len(a.haveLetters)
+	for i := 0; i < n; i++ {
+		if have == a.haveLetters[i] {
+			x := ((float64(i)-.5*float64(n))*.03 + .5) * a.width
+			a.ctx.Call("setTransform", s, 0, 0, s, x, y/2)
+			text := string(rune(alphabetLetter(i)))
+			a.ctx.Call("fillText", text, 0, 0)
 		}
 	}
-	ctx.Call("beginPath")
-	ctx.Call("moveTo", px[0], py[0])
-	ctx.Call("lineTo", px[1], py[1])
-	ctx.Call("lineTo", px[2], py[2])
-	ctx.Call("fill")
+	a.ctx.Call("restore")
 }
 
 func (a *anim) drawBar() {
 	a.ctx.Set("fillStyle", barColor)
-	a.ctx.Call("fillRect", 0, 0, a.width, a.buttonHeight())
-	a.ctx.Set("fillStyle", buttonColor)
-	leftX := 0.0
-	for i := 0; i < 3; i++ {
-		rightX := a.buttonRight(i)
-		center := .5 * (leftX + rightX)
-		if i == 0 && a.running {
-			y := a.buttonHeight()
-			a.ctx.Call("fillRect", center-y/2, y/4, y/2, y/2)
-			a.ctx.Call("fillRect", center+y/4, y/4, y/2, y/2)
-		} else {
-			drawTriangle(a.ctx, i, center, a.buttonHeight())
-		}
-		leftX = rightX
-	}
+	a.ctx.Call("fillRect", 0, 0, a.width, a.barHeight())
+	a.drawLettersHave(false, barTextColor)
+	a.drawLettersHave(true, barTextHighColor)
 }
 
 func (a *anim) drawControl() {
@@ -263,52 +245,48 @@ func (a *anim) drawControl() {
 		x -= w
 	}
 	a.ctx.Set("fillStyle", color)
-	y := a.buttonHeight()
+	y := a.barHeight()
 	a.ctx.Call("fillRect", x, y, w, y*.5)
 }
 
 func (a *anim) clear() {
 	a.ctx.Set("fillStyle", voidColor)
-	a.ctx.Call("fillRect", 0, a.buttonHeight(), a.width, a.height)
+	a.ctx.Call("fillRect", 0, a.barHeight(), a.width, a.height)
 	a.drawBar()
 }
 
 func (a *anim) drawDot(i int) {
 	d := a.dots[i]
-	if !a.running || i == a.selectedDot || i >= a.nGassDots {
+	b := d.Angle
+	if i >= a.iLetterDots {
 		r := d.R
-		if a.running {
-			r *= 1.1
-		}
+		a.ctx.Set("fillStyle", letterCupColor)
 		a.ctx.Call("beginPath")
-		a.ctx.Call("arc", d.X-a.viewX, d.Y, r, 0, math.Pi*2)
+		a.ctx.Call("arc", d.X-a.viewX, d.Y, r, b, math.Pi+b)
 		a.ctx.Call("fill")
 		a.ctx.Call("closePath")
 	}
-	if a.running {
-		img := a.images[0]
-		b := d.Angle
-		if i >= a.nGassDots {
-			letter := string(rune(65 + (i - a.nGassDots)))
-			s := d.R / 9
-			a.ctx.Call("save")
-			a.ctx.Set("fillStyle", "white")
-			a.ctx.Call("setTransform", s, 0, 0, s, d.X-a.viewX, d.Y)
-			a.ctx.Call("rotate", b)
-			a.ctx.Call("fillText", letter, -5, 4)
-			a.ctx.Call("restore")
-			return
-		}
-		if i < a.nWheels {
-			img = a.images[1]
-			b += a.wheels[i].angle // alt: =
-		}
+	img := a.images[0]
+	if i >= a.iLetterDots {
+		text := string(rune(alphabetLetter(a.alienLetters[i-a.iLetterDots])))
+		s := d.R / 9
+		a.ctx.Set("fillStyle", letterColor)
 		a.ctx.Call("save")
-		a.ctx.Call("translate", d.X-a.viewX, d.Y)
+		a.ctx.Call("setTransform", s, 0, 0, s, d.X-a.viewX, d.Y)
 		a.ctx.Call("rotate", b)
-		a.ctx.Call("drawImage", img, -d.R, -d.R, d.R*2, d.R*2)
+		a.ctx.Call("fillText", text, -5, 4)
 		a.ctx.Call("restore")
+		return
 	}
+	if i < a.nWheels {
+		img = a.images[1]
+		b += a.wheels[i].angle // alt: =
+	}
+	a.ctx.Call("save")
+	a.ctx.Call("translate", d.X-a.viewX, d.Y)
+	a.ctx.Call("rotate", b)
+	a.ctx.Call("drawImage", img, -d.R, -d.R, d.R*2, d.R*2)
+	a.ctx.Call("restore")
 }
 
 func (a *anim) drawLineTo(i int, x, y, k float64) {
@@ -322,26 +300,17 @@ func (a *anim) drawLineTo(i int, x, y, k float64) {
 
 func (a *anim) drawView() {
 	a.clear()
-	a.ctx.Set("font", "15px Arial")
+	a.ctx.Set("strokeStyle", lineColor)
 	for i := 0; i < a.nDots; i++ {
 		from := a.dots[i]
-		if i == a.selectedDot && !a.running {
-			a.ctx.Set("strokeStyle", selectedLineColor)
-		} else {
-			a.ctx.Set("strokeStyle", lineColor)
-		}
 		for _, s := range from.Springs {
 			a.drawLineTo(i, s.To.X, s.To.Y, s.K)
 		}
 	}
 	for i := 0; i < a.nDots; i++ {
-		if i == a.selectedDot {
-			a.ctx.Set("fillStyle", selectedDotColor)
-		} else {
-			a.ctx.Set("fillStyle", dotColor)
-		}
 		a.drawDot(i)
 	}
+
 	a.drawPlatforms()
 }
 
@@ -361,8 +330,8 @@ func (a *anim) drawPlatforms() {
 func (a *anim) platformsStep(deltaT float64) {
 	for i := 0; i < a.nDots; i++ {
 		d := &a.dots[i]
-		if i >= a.nWheels && i < a.nGassDots {
-			continue // note: avoid car hooked stuck
+		if i >= a.nWheels && i < a.iLetterDots {
+			continue
 		}
 		hOn := 0.
 		jOn := 0
@@ -434,6 +403,25 @@ func (a *anim) wheelRotation(i int) {
 	w.angle += w.angleVelocity * a.deltaT
 }
 
+func (a *anim) wheelsStep() {
+	for i := 0; i < a.nWheels; i++ {
+		a.wheelRotation(i)
+	}
+}
+
+func (a *anim) lettersStep() {
+	for i := a.iLetterDots; i < a.nDots; i++ {
+		u := a.alienLetters[i-a.iLetterDots]
+		if !a.haveLetters[u] {
+			d := &a.dots[i]
+			c := &a.dots[a.nCarDots-1]
+			if distanceXY(c.X-d.X, c.Y-d.Y) < a.dotSize*3 {
+				a.haveLetters[u] = true
+			}
+		}
+	}
+}
+
 func (a *anim) viewBorderStep() {
 	for i := 0; i < a.nCarDots; i++ {
 		d := &a.dots[i]
@@ -465,20 +453,21 @@ func (a *anim) viewScrollStep() {
 func (a *anim) platformCycle(i int) {
 	leftX := a.viewX + (1+a.rands.Float64())*a.width
 	rightX := leftX + (1+a.rands.Float64())*.2*a.width
-	leftY := (.65+a.rands.Float64())*.75*a.height
+	leftY := (.65 + a.rands.Float64()) * .75 * a.height
 	rightY := leftY + (a.rands.Float64()-.5)*.4*a.height
-	height := (1+a.rands.Float64())*.4*a.dotSize
+	height := (1 + a.rands.Float64()) * .4 * a.dotSize
 	a.platforms[i] = newPlatform(leftX, rightX, leftY, rightY, height)
 }
 
 func (a *anim) alienCycle(i int) {
 	d := &a.dots[i]
-	x := a.viewX + (a.rands.Float64())*a.width
+	a.alienLetters[i-a.iLetterDots] = a.rands.Intn(len(a.haveLetters))
+	x := a.viewX + (1+a.rands.Float64())*a.width + a.dotSize
 	y := 0.
 	h := a.dotSize * 2
 	for d != nil {
-		d.X = x + a.rands.Float64() * a.dotSize * .125
-		d.Y = y + a.rands.Float64() * a.dotSize * .125
+		d.X = x + a.rands.Float64()*a.dotSize*.125
+		d.Y = y + a.rands.Float64()*a.dotSize*.125
 		y += h
 		if len(d.Springs) != 0 {
 			d.Springs[0].Distance = h
@@ -495,7 +484,7 @@ func (a *anim) worldCycle() {
 			a.platformCycle(i)
 		}
 	}
-	for i := a.nGassDots; i < a.nDots; i++ {
+	for i := a.iLetterDots; i < a.nDots; i++ {
 		if a.dots[i].X < a.viewX-a.width*.5 || a.dots[i].Y > a.height*1.5 {
 			a.alienCycle(i)
 		}
@@ -503,274 +492,63 @@ func (a *anim) worldCycle() {
 }
 
 func (a *anim) appendAliens() {
-	nAliens := 7
+	nAliens := 5
 	nBody := 2 // minimum: 1
-	n := a.nCarDots + nAliens * (1 + nBody)
+	n := a.nDots + nAliens*(1+nBody)
 	if n >= len(a.dots) {
-		return  // overflow: skip aliens
+		return // overflow: skip aliens
 	}
-	a.nDots = n
-	a.nGassDots = n - nAliens
-	for i := a.nCarDots; i < a.nDots; i++ {
-		m := defaultMass
-		r := a.dotRadius(m)
-		a.dots[i] = springweb.NewNode(0, 0, r, m)
+	a.iLetterDots = n - nAliens
+	for q := a.nDots; q < n; q++ {
+		a.newDot(0, 0)
 	}
-	j := a.nGassDots
+	j := a.iLetterDots
 	i := a.nDots
-	for i > a.nGassDots {
+	for i > a.iLetterDots {
 		i--
-		k := defaultK
 		j--
-		a.dots[i].NewSpring(&a.dots[j], k, armKFactor*k)
+		a.newLine(i, j)
 		for q := 1; q < nBody; q++ {
-			a.dots[j].NewSpring(&a.dots[j-1], k, armKFactor*k)
+			a.newLine(j, j-1)
 			j--
 		}
 		a.alienCycle(i)
 	}
 }
 
-func (a *anim) toggleRunEdit() {
-	a.running = !a.running
-	if a.running {
-		a.resetNodes = make([]springweb.Node, a.nDots)
-		a.resetViewX = a.viewX
-		copy(a.resetNodes, a.dots)
-		springweb.StepsPrepare(a.dots[:a.nDots])
-		a.lastCall = time.Now()
-		a.nCarDots = a.nDots
-		a.appendAliens()
-		js.Global().Call("requestAnimationFrame", a.callback)
-	} else {
-		copy(a.dots, a.resetNodes)
-		a.nDots = a.nCarDots
-		for i := 0; i < a.nDots; i++ {
-			a.dots[i].X += a.viewX - a.resetViewX
-		}
-		a.selectedDot = a.nDots - 1
-		a.drawView()
-	}
-}
-
-func (a *anim) editClickVoid(x, y float64) {
-	if a.nDots == len(a.dots) {
-		return
-	}
-	a.selectedDot = a.nDots
-	a.newDot(x, y)
-	a.drawView()
-}
-
-func (a *anim) editClickDot(i int) {
-	j := a.nDots - 1
-	d := &a.dots[j]
-	if i == j {
-		a.nDots--
-		if j != 0 {
-			a.selectedDot = j - 1
-		}
-		return
-	}
-	for k, _ := range d.Springs {
-		if d.Springs[k].To == &a.dots[i] {
-			d.Springs = append(d.Springs[:k], d.Springs[k+1:]...)
-			return
-		}
-	}
-	a.newLine(j, i)
-}
-
-func (a *anim) lastK() float64 {
-	for i := a.nDots - 1; i >= 0; i-- {
-		s := a.dots[i].Springs
-		n := len(s)
-		if n != 0 {
-			return s[n-1].K
-		}
-	}
-	return defaultK
-}
-
-func (a *anim) lastMass() float64 {
-	if a.nDots == 0 {
-		return defaultMass
-	}
-	return a.dots[a.nDots-1].M
-}
-
-func (a *anim) clickRunning(x, y float64) {
-	if i := a.findDot(x, y); i >= 0 {
-		a.selectedDot = i
-	}
-	a.dragging = true
-	a.positionDraggedDot(x, y)
-}
-
-func (a *anim) positionDraggedDot(x, y float64) {
-	if a.dragging {
-		node := &a.dots[a.selectedDot]
-		if a.deltaT > 0 {
-			node.VelocityX = .1 * (9*node.VelocityX + (x-node.X)/a.deltaT)
-			node.VelocityY = .1 * (9*node.VelocityY + (y-node.Y)/a.deltaT)
-		}
-		node.X = x
-		node.Y = y
-	}
-}
-
-func (a *anim) draggedDotPosition() (x, y float64) {
-	if a.dragging {
-		node := a.dots[a.selectedDot]
-		x = node.X
-		y = node.Y
-	}
-	return
-}
-
-func (a *anim) dotSelect(z float64) {
-	if z > 0 {
-		a.selectedDot++
-		if a.selectedDot == a.nDots {
-			a.selectedDot = 0
-		}
-	} else {
-		if a.selectedDot == 0 {
-			a.selectedDot = a.nDots
-		}
-		a.selectedDot--
-	}
-}
-
-func (a *anim) sizeCurrent(z float64) {
-	if a.nDots <= 0 {
-		return
-	}
-	j := a.nDots - 1
-	d := a.dots[j]
-	n := len(d.Springs)
-	if n != 0 {
-		s := &d.Springs[n-1]
-		k := s.K * (1 - z*sizeFactor)
-		if k >= minK && k <= maxK {
-			s.K = k
-		}
-	} else {
-		d := &a.dots[j]
-		w := d.M / (1 + z*sizeFactor)
-		if w >= minMass && w <= maxMass {
-			d.M = w
-			d.R = a.dotRadius(d.M)
-		}
-	}
-	a.drawView()
-}
-
-func (a *anim) upDown(z float64) {
-	if a.running {
-		a.dotSelect(z)
-	} else {
-		a.sizeCurrent(z)
-	}
-}
-
-func (a *anim) clickButton(x float64) {
-	if x < a.buttonRight(0) {
-		a.toggleRunEdit()
-	} else if x < a.buttonRight(1) {
-		a.upDown(+sizeButtonClick)
-	} else {
-		a.upDown(-sizeButtonClick)
-	}
-}
-
-func (a *anim) click(event js.Value) {
-	x := event.Get("clientX").Float()
-	y := event.Get("clientY").Float()
-	if y < a.buttonHeight() {
-		a.clickButton(x)
-		return
-	}
-	x += a.viewX
-
-	if a.running {
-		a.clickRunning(x, y)
-		return
-	}
-
-	i := a.findDot(x, y)
-	if i >= 0 {
-		a.editClickDot(i)
-		a.drawView()
-		return
-	}
-
-	a.editClickVoid(x, y)
-}
-
-func (a *anim) pointerWheel(event js.Value) {
-	z := event.Get("deltaY").Float()
-	a.upDown(z)
-}
-
-func (a *anim) pointerRelease(event js.Value) {
-	a.dragging = false
+func (a *anim) start() {
+	springweb.StepsPrepare(a.dots[:a.nDots])
+	a.lastCall = time.Now()
+	a.nCarDots = a.nDots
+	a.appendAliens()
+	js.Global().Call("requestAnimationFrame", a.callback)
 }
 
 func (a *anim) pointerMove(event js.Value) {
-	if !a.running {
-		return
-	}
 	x := event.Get("clientX").Float()
-	if a.dragging {
-		y := event.Get("clientY").Float()
-		a.positionDraggedDot(x+a.viewX, y)
-	}
 	a.wheelForce = (2*x/a.width - 1) * maxWheelForce
-}
-
-func (a *anim) keyup(event js.Value) {
-	a.keyisdown = false
-}
-
-func (a *anim) keydown(event js.Value) {
-	if a.keyisdown {
-		return
-	}
-	a.keyisdown = true
-	switch event.Get("code").String() {
-	case "ArrowDown":
-		event.Call("preventDefault")
-		a.upDown(+sizeButtonClick)
-	case "ArrowUp":
-		event.Call("preventDefault")
-		a.upDown(-sizeButtonClick)
-	case "ArrowRight":
-		event.Call("preventDefault")
-		a.toggleRunEdit()
-	}
 }
 
 func main() {
 	height := js.Global().Get("innerHeight").Float() - 24
 	width := js.Global().Get("innerWidth").Float() - 24
-	a := newAnim(width, height, height*0.05, 128)
-	eventHandlers := map[string]func(js.Value){
-		"pointerdown": a.click,
-		"wheel":       a.pointerWheel,
-		"pointerup":   a.pointerRelease,
-		"pointermove": a.pointerMove,
-		"keyup":       a.keyup,
-		"keydown":     a.keydown,
-	}
-	for eventName, f := range eventHandlers {
-		handler := f
-		js.Global().Call("addEventListener", eventName,
-			js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-				handler(args[0])
-				return nil
-			}))
-	}
+	a := newAnim(width, height, height*0.05, 24)
+	js.Global().Call("addEventListener", "pointermove",
+		js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+			a.pointerMove(args[0])
+			return nil
+		}))
+
+	h := 2 * a.dotSize
+	a.newDot(h, a.height-h)
+	a.newDot(h*3, a.height-h)
+	a.newDotM(h*(1+a.vary()), a.height-h*1.5, minMass)
+	a.newLine(1, 0)
+	a.newLineK(2, 1, minK)
+	a.newLineK(2, 0, minK)
+	a.start()
+	log("h", height)
+	log("w", width)
 
 	<-make(chan bool)
 }
